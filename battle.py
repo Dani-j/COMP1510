1bc2b1b68359775a3ec3c64785e0063c0a634c75
Jobcrazy <xianip@163.com>
2020-10-23 20:18:40 +0000

    James: Add constant variables for battle.py

--- battle.py
+++ battle.py
@@ -14,70 +14,75 @@ import doctest
 
 
 def FLEE_FAIL():
-    """
+  """
+  A constant integer that represents the charactor was attacked while fleeing
 
-    :precondition:
-    :postcondition:
-    :return:
-    """
-    return 1
+  :precondition: None
+  :postcondition: Always return 1
+  :return: A integer that represents the charactor was attacked while fleeing
+  """
+  return 1
 
 def ESCAPED():
-    """
+  """
+  A constant integer that represents the charactor has eacaped from a battle
 
-    :precondition:
-    :postcondition:
-    :return:
-    """
-    return 99
+  :precondition: None
+  :postcondition: Always return 99
+  :return: A integer that represents the charactor has eacaped from a battle
+  """
+  return 99
 
 
 def DRAW_GAME():
-    """
+  """
+  A constant integer that represents a draw game
 
-    :precondition:
-    :postcondition:
-    :return:
-    """
-    return 0
+  :precondition: None
+  :postcondition: Always return 0
+  :return: A integer that represents a draw game
+  """
+  return 0
 
 
 def CHARACTER_DIE():
-    """
+  """
+  A constant integer that represents the character has died
 
-    :precondition:
-    :postcondition:
-    :return:
-    """
-    return 1
+  :precondition: None
+  :postcondition: Always return 1
+  :return: A integer that represents the character has died
+  """
+  return 1
 
 
 def MONSTER_DIE():
-    """
+  """
+  A constant integer that represents a monster has died
 
-    :precondition:
-    :postcondition:
-    :return:
-    """
-    return 2
+  :precondition: None
+  :postcondition: Always return 1
+  :return: A integer that represents a monster has died
+  """
+  return 2
 
 
 def fight_ready():
-    """
+  """
 
-    :precondition:
-    :postcondition:
-    :return:
-    """
-    # 猜拳扣血
-    return random.randint(1, 2)
+  :precondition:
+  :postcondition:
+  :return:
+  """
+  # 猜拳扣血
+  return random.randint(1, 2)
 
 
 def main():
-    """Drive the program."""
-    doctest.testmod()
+  """Drive the program."""
+  doctest.testmod()
 
 
 if __name__ == '__main__':
-    main()
+  main()
 
