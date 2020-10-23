728f6687dbc913d8d159482c459c6f9df25f7780
Jobcrazy <xianip@163.com>
2020-10-23 18:34:35 +0000

    Create the basic functions for character information

--- battle.py
+++ battle.py
@@ -1,9 +1,10 @@
 """
-(Description:)
+This file contains the functions related to battle operations and results.
 
 Authors: James (Hang) Liu, Dani (Danfeng) Jin
 Date: October 23, 2020
 """
+
 
 import random
 import user
@@ -70,63 +71,6 @@ def fight_ready():
     """
     # 猜拳扣血
     return random.randint(1, 2)
-
-
-def attack(character_choice, monster_choice, character_info, monster_info):
-    """
-
-
-    :param character_choice:
-    :param monster_choice:
-    :param character_info:
-    :param monster_info:
-    :precondition:
-    :postcondition:
-    :return:
-    """
-    if character_choice < monster_choice:
-        message.character_hurted(monster_info, monster_choice)
-        if user.hurt(character_info, monster_choice):
-            message.character_die(monster_info)
-            return CHARACTER_DIE()
-    elif character_choice > monster_choice:
-        message.monster_hurted(monster_info, character_choice)
-        if monster.hurt(monster_info, character_choice):
-            message.monster_die(monster_info)
-            return MONSTER_DIE()
-    else:
-        message.monster_hurted(monster_info, character_choice)
-        return DRAW_GAME()
-
-
-def start(character_info, monster_info, action):
-    """
-
-
-    :param character_info:
-    :param monster_info:
-    :param action:
-    :precondition:
-    :postcondition:
-    :return:
-    """
-    battle_result = DRAW_GAME()
-    if "kill" == action:
-        while character_info[user.HEALTH()] > 0 and monster_info[monster.HEALTH()] > 0:
-            character_choice = fight_ready()
-            monster_choice = fight_ready()
-            battle_result = attack(character_choice, monster_choice, character_info, monster_info)
-        if 0 < character_info[user.HEALTH()]:
-            message.character_health(character_info)
-        return battle_result
-    else:
-        if FLEE_FAIL() == random.randint(1, 1):
-            hurt_amout = random.randint(1, 4)
-            user.hurt(character_info, hurt_amout)
-            message.character_hurted(monster_info, hurt_amout)
-            if 0 < character_info[user.HEALTH()]:
-                message.character_health(character_info)
-            return ESCAPED()
 
 
 def main():
---
+++ user.py
@@ -0,0 +1,117 @@
+"""
+This file contains the functions related to character information.
+
+Authors: James (Hang) Liu, Dani (Danfeng) Jin
+Date: October 23, 2020
+"""
+
+
+import random
+import map
+import message
+import doctest
+
+
+def CHARACTERNAME():
+    """
+    Return the index of character name element in a character information list.
+
+    :precondition: None
+    :postcondition: Return the index where the character name is stored in the character_info list
+    :return: An integer, the index of the character name in a character information list
+    """
+    return 0
+
+
+def HEALTH():
+    """
+    Return the index of health point element in a character information list.
+
+    :precondition: None
+    :postcondition: Return the index where the health point is stored in the character_info list
+    :return: An integer, the index of the health point in a character information list
+    """
+    return 1
+
+
+def POSITION():
+    """
+    Return the index of the position element in a character information list.
+
+    :precondition: None
+    :postcondition: Return the index where the position element is stored in the character_info list
+    :return: An integer, the index of the position in a character information list
+    """
+    return 2
+
+
+def MAX_HEALTH():
+    """
+    Return the maximum health point of a character.
+
+    :precondition: None
+    :postcondition: Return the maximum health point of a character
+    :return: An integer, the the maximum health point of a character
+    """
+    return 10
+
+
+def get_character_name(character_info):
+    """
+    Get the character name from a character information list.
+
+    :param character_info: A list that contains a character information
+    :precondition: The character_info must contains a list contains character information
+    :postcondition: Return the correct character name
+    :return: A string, represents the character name
+
+    >>> character_info = ["James", 10, [1, 2]]
+    >>> get_character_name(character_info)
+    'James'
+    """
+    return character_info[CHARACTERNAME()]
+
+
+def get_character_position(character_info):
+    """
+    Return the character coordination
+
+    :param character_info: A list that contains a character information
+    :precondition: The character_info must contains a list contains character information
+    :postcondition: Return the correct character coordination
+    :return: A list contains the character coordination
+
+    >>> character_info = ["DJ", 10, [1, 2]]
+    >>> get_character_position(character_info)
+    [1, 2]
+    """
+    return character_info[POSITION()]
+
+
+def create_character():
+    character_info = []
+    return character_info
+
+
+def heal(character_info):
+
+
+def hurt(character_info, amount):
+
+
+def move(character_info, direction, game_map):
+
+
+def random_move(character_info, game_map):  # remove this function since we won't use it?
+
+
+def get_character_choice(tip):
+
+
+def main():
+    """Drive the program."""
+    doctest.testmod(verbose=True)
+
+
+if __name__ == '__main__':
+    main()
