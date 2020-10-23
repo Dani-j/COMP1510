f50bddb937fd857a3cb6b123d2cf870af2f23a6b
Jobcrazy <xianip@163.com>
2020-10-23 18:11:25 +0000

    Create and show map functions finished

---
+++ monster.py
@@ -0,0 +1,80 @@
+import random
+import doctest
+
+def MONSTER_NAME():
+    """
+    Return the index of monster name in the monster information list.
+
+    :precondition: none
+    :postcondition: return the constant index where the moster name is stored 
+      in the moster information list
+    :return: an integer, the constant index of monster name
+    """
+    return 0
+
+
+def HEALTH():
+    """
+    Return the index of monster HP in the monster information list.
+
+    :precondition: none
+    :postcondition: return the constant index where the moster HP is stored 
+      in the moster information list
+    :return: an integer, the constant index of monster HP
+    """
+    return 1
+
+
+def MONSTER_EXISTS():
+    """
+    Return if monster exists
+
+    :precondition: none
+    :postcondition: return the a constant integer showing if the monster exists or not
+    :return: an integer, determine if a monster exists
+    """
+    return 1
+
+
+def create_monster():
+    """
+    Create a monster with random name randomly
+
+    :precondition: none
+    :postcondition: return a random monster name or return nothing
+    :return: monster name or none
+    """
+    # if MONSTER_EXISTS() != random.randint(1, 25):
+    x = random.randint(1, 2) 
+    if MONSTER_EXISTS() != x:  # x != 1, a monster won't appear
+        return None
+
+    names = ["Dragon", "Zombie", "Python", "Jack the Ripper"]  # will be more monsters if we have more time
+    monster = random.choice(names)  # randomly generate a monster
+    return monster
+
+
+def hurt(monster_info, amount):
+    """
+    Decrease the monster HP and check if monster HP is smaller than 0 or not.
+
+    :param monster_info: a 
+    :param amount:
+    :precondition:
+    :postcondition:
+    :return:
+
+    >>> 
+    """
+    # return False if character is dead
+    monster_info[HEALTH()] -= amount
+    return monster_info[HEALTH()] <= 0
+
+
+def main():
+    """Test the module."""
+    doctest.testmod()
+
+
+if __name__ == '__main__':
+    main()
